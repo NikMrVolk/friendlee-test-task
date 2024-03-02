366a1b080c5cb68bbf555d82b6b94bf1d67eeb8b
NickZavadski <nick.mr.volk@gmail.com>
2024-03-02 21:10:59 +0300

    feat: block wrapper component

---
+++ src/components/common/BlockWrapper.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BlockWrapper({children}){
+   return (
+      <div className="block-wrapper">
+         {children}
+      </div>
+   )
+}
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
+import BlockWrapper from '../components/common/BlockWrapper'
 import Mouse from '../components/common/Mouse'
 
 export default function Main() {
     return (
-        <div className="page">
+        <div className="main-page">
+            <header>
+                <BlockWrapper>
+                    <ul className="list">
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                        <li>4</li>
+                    </ul>
+                </BlockWrapper>
+            </header>
             <Mouse />
         </div>
     )
