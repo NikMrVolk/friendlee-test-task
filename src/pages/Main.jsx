65b66f32e77553a582ddc0c219b71e5637e3afbe
NickZavadski <nick.mr.volk@gmail.com>
2024-03-06 01:59:52 +0300

    prog: text block to description section

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,6 +4,7 @@ import Mouse from '../components/common/Mouse'
 import Header from '../components/header/Header'
 import FirstScreen from '../components/sections/FirstScreen'
 import { useMouseHover } from '../hooks/useMouseHover'
+import EyeSvg from '../components/svg/EyeSvg'
 
 export default function Main() {
     const [isBurgerActive, setIsBurgerActive] = useState(false)
@@ -19,7 +20,14 @@ export default function Main() {
             />
             <main>
                 <FirstScreen />
-                <section className="description"></section>
+                <section className="description block-wrapper">
+                    <div className="description__body">
+                        <p className="description__text">
+                            Мы создаем <EyeSvg className="description__svg" />
+                            уникальные бренды основываясь глубоком понимании потребителей
+                        </p>
+                    </div>
+                </section>
             </main>
             <Mouse isMouseHover={isMouseHover} />
         </div>
